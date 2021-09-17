af0f5118592e492fe502c44d321bc35751abb523
Rafael Munoz <rafael.munoz77@gmail.com>
2021-09-17 09:23:53 +0200

    Error de charset a UTF-8

--- src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
+++ src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
@@ -3,7 +3,7 @@ package tutorialJava.capitulo1;
 public class Ejemplo01_HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo, 1º DAW, qué tal?"); // Esto es un comentario
+		System.out.println("Hola Mundo, 1Âº DAW, quÃ© tal?"); // Esto es un comentario
 	}     
 
 }
