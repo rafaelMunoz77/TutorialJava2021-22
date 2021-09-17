a2a76c9e8b254dddf62f71febcc92950cc9b9d7b
Rafael Munoz <rafael.munoz77@gmail.com>
2021-09-17 08:54:42 +0200

    Ejemplo de modificación de fichero en clase

--- src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
+++ src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
@@ -3,7 +3,7 @@ package tutorialJava.capitulo1;
 public class Ejemplo01_HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo"); // Esto es un comentario
+		System.out.println("Hola Mundo, 1º DAW"); // Esto es un comentario
 	}     
 
 }
